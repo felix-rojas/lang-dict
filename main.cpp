130bed2552da0851d0b420d8fd3a4c63b3c5bfcd
felix-rojas <a01201946@tec.mx>
2023-09-15 14:17:21 -0600

    add user menu

--- main.cpp
+++ main.cpp
@@ -1,65 +1,89 @@
 #include <cmath>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <ostream>
 #include <regex>
 #include <stdexcept>
 #include <string>
 
-const std::regex entry("<entry form=\"([^\"]*)\"");
+const std::regex entry("<entry[^>]* form=\"([^\"]+)\"");
 const std::regex entry_end("<\\/entry>");
 // Define the regex pattern
 const std::regex pattern("<([^>]+)>(.*?)<\\/\\1>");
 
 const std::string filepath = "./wiktionaryXfr2010.xml";
 
+const std::string def = "gloss";
+const std::string etym = "etym";
+const std::string eg = "example";
+
 std::ifstream loadFile(std::string filepath) {
   // Load xml file
   std::ifstream dict_file(filepath);
   if (!dict_file) {
-    throw std::runtime_error("File could not be loaded or doesnt exist.");
+    throw std::runtime_error("File could not be loaded or does not exist.");
   }
   return dict_file;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
+  unsigned int n = 10000;
+  std::cout << "How many entries would you like to process? " << std::endl;
+  std::cin >> n;
+  int choice = 1;
+
+  std::cout << "Menu:" << std::endl;
+  std::cout << "1. Word" << std::endl;
+  std::cout << "2. Word and Definition" << std::endl;
+  std::cout << "3. Word, Etymology and Definition" << std::endl;
+  std::cout << "Enter your choice (1-3), any other choice exits: ";
+  std::cin >> choice;
+  if (choice >= 4 || choice <= 0) {
+    std::cout << "Exiting..." << std::endl;
+    exit(1);
+  }
   int i = 0;
   std::string line_data;
   auto dict_file = loadFile(filepath);
 
   // no parameters
-  if (argc < 2) {
-    // getline has linear time O(n)
-    while (getline(dict_file, line_data) && i < 400) {
-      // get the word/phrase to process
-      std::sregex_iterator iter(line_data.begin(), line_data.end(), pattern);
-      std::sregex_iterator
-          end; // Create an iterator to find all matches in the input string
+  // getline has linear time O(n)
+  while (getline(dict_file, line_data) && i <= n) {
+    // get the word/phrase to process
+    std::sregex_iterator iter(line_data.begin(), line_data.end(), entry);
+    std::sregex_iterator iter2(line_data.begin(), line_data.end(), pattern);
+    // Create an iterator to find all matches in the input string
+    // regex_search is a O(n) operation on each processed line_data
+    // yes, this means O(n^2)
+    // it processes the regex expression using Thompsons algorithm
 
-      std::smatch match; // generate a string match type to store results
-      // regex_search is a O(n) operation on each processed line
-      // it processes the regex expression using Thompsons algorithm
-      if (std::regex_search(line_data, match, entry)) {
-        std::cout << match[1] << "\t";
-      }
-
-      // Loop through the matches and extract the content between tags
-      while (iter != end) {
-        std::smatch match = *iter;
-        if (match[1] == "gloss") {
-          std::cout << match[2] << "\t";
-        }
-        ++iter;
-      }
-
-      // separate word definitions
-      if (std::regex_search(line_data, match, entry_end)) {
+    // print word
+    for (auto it = iter; it != std::sregex_iterator(); ++it) {
+      if (i != n) {
         std::cout << std::endl;
-        i++;
+        std::cout << it->str(1) << "\t";
       }
+      i++;
     }
 
-    dict_file.close();
-    return 0;
+    switch (choice) {
+    case 2:
+      for (auto it2 = iter2;
+           it2 != std::sregex_iterator() && it2->str(1) == def; ++it2) {
+        std::cout << it2->str(2) << " ";
+      }
+      break;
+    case 3:
+      for (auto it2 = iter2; it2 != std::sregex_iterator() &&
+                             ((it2->str(1) == def) || (it2->str(1) == etym));
+           ++it2) {
+        std::cout << it2->str(2) << " ";
+      }
+      break;
+    }
   }
+  dict_file.close();
+  return 0;
 }
