0fe0e9f48ab7443b04d1a3a311c0aa731dd4e59c
felix-rojas <a01201946@tec.mx>
2023-09-17 15:27:56 -0600

    add search and definition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <cmath>
-#include <cstdlib>
+#include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -7,153 +7,190 @@
 #include <regex>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
-const std::regex entry("<entry[^>]* form=\"([^\"]+)\"");
-const std::regex entry_end("<\\/entry>");
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::regex;
+using std::sregex_iterator;
+using std::string;
+using std::vector;
+
+const regex entry("<entry[^>]* form=\"([^\"]+)\"");
+const regex entry_end("<\\/entry>");
 // Define the regex pattern
-const std::regex pattern("<([^>]+)>(.*?)<\\/\\1>");
+const regex pattern("<([^>]+)>(.*?)<\\/\\1>");
 
-const std::string filepath = "./wiktionaryXfr2010.xml";
+const string filepath = "./wiktionaryXfr2010.xml";
 
-const std::string def = "gloss";
-const std::string etym = "etym";
-const std::string eg = "example";
+const string def = "gloss";
+const string etym = "etym";
+const string eg = "example";
 
-std::ifstream loadFile(const std::string& filepath) {
+ifstream loadFile(const string &filepath) {
   // Load xml file
-  std::ifstream dict_file(filepath);
+  ifstream dict_file(filepath);
   if (!dict_file) {
     throw std::runtime_error("File could not be loaded or does not exist.");
   }
   return dict_file;
 }
 
+
 struct Word {
-  std::string word;
-  std::vector<std::string> etymology;
-  std::vector<std::string> definition;
-  Word(std::string w, std::string d, std::string e) {
+  int index;
+  string word;
+  Word(const string &w, const int &i) {
     this->word = w;
-    this->definition.push_back(d);
-    this->etymology.push_back( e);
+    this->index = i;
   }
-  Word(std::string w, std::string d) {
-    this->word = w;
-    this->definition.push_back(d);
-  }
-  Word(std::string w) {
-    this->word = w;
-  }
-  void add_definition(std::string def){
-    this->definition.push_back(def);
-  }
-  void add_etymology(std::string etym){
-    
-    this->etymology.push_back(etym);
-  }
+  bool operator<(const Word &other) const {
+        return this->word < other.word;
+    }
 };
 
-  unsigned int n = 10000;
-  int choice = 1;
-  std::vector<Word> dictionary;
-  
+bool compareWord(const Word &a, const Word &b) { return a.word < b.word; }
+
+struct Definition {
+  string definition;
+  Definition(const string &w) { this->definition = w; }
+};
+
+// bug: std library compares using ascii values
+// diacritic words have a higher ascii value
+
+// Perform binary search
+int findWord(const std::vector<Word> &wordVector, const std::string &target) {
+    int left = 0;
+    int right = wordVector.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (wordVector[mid].word == target) {
+            return wordVector[mid].index; // Word found, return its index
+        } else if (wordVector[mid].word < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+vector<Word> dictionary;
+
+// O(n) gets the entire line until it finds the number of line
+std::string searchDefinition(int desiredRowNumber) {
+  std::ifstream file("dict.tsv"); // Open the file
+  if (!file.is_open()) {
+    return "Could not open the file.";
+  }
+  std::string line;
+  int currentRowNumber = 0;
+  while (std::getline(file, line) && currentRowNumber != desiredRowNumber) {
+    currentRowNumber++;
+  }
+  return line;
+  file.close(); // Close the file
+
+  if (currentRowNumber != desiredRowNumber) {
+    return "Not found";
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-  std::cout << "How many entries would you like to process? " << std::endl;
-  std::cin >> n;
+    cout << "Default run, 100 entries \n";
+    int n = 100;
+    int i = 0;
 
-  std::cout << "Menu:" << std::endl;
-  std::cout << "1. Word" << std::endl;
-  std::cout << "2. Word and Definition" << std::endl;
-  std::cout << "3. Word, Etymology and Definition" << std::endl;
-  std::cout << "Enter your choice (1-3), any other choice exits: ";
-  std::cin >> choice;
-  if (choice >= 4 || choice <= 0) {
-    std::cout << "Exiting..." << std::endl;
-    exit(1);
-  }
-  
-  int i = 0;
-  std::string line_data;
-  auto dict_file = loadFile(filepath);
+    string line_data;
+    string temp;
+    auto dict_file = loadFile(filepath);
+    ofstream file("dict.tsv");
 
-  // no parameters
-  // getline has linear time O(n)
-  while (getline(dict_file, line_data) && i <= n) {
-    // get the word/phrase to process
-    std::sregex_iterator iter(line_data.begin(), line_data.end(), entry);
-    std::sregex_iterator iter2(line_data.begin(), line_data.end(), pattern);
-    // Create an iterator to find all matches in the input string
-    // regex_search is a O(n) operation on each processed line_data
-    // yes, this means O(n^2)
-    // it processes the regex expression using Thompsons algorithm
+    // no parameters
+    // getline has linear time O(n)
+    while (getline(dict_file, line_data) && i < n) {
+      sregex_iterator iter(line_data.begin(), line_data.end(), entry);
+      sregex_iterator iter2(line_data.begin(), line_data.end(), pattern);
 
-    // print word
-    for (auto it = iter; it != std::sregex_iterator(); ++it) {
-      if (i != n) {
-        std::cout << std::endl;
-        std::cout << it->str(1) << "\t";
-        it->str(1);
-      }
-      i++;
-    }
-
-    switch (choice) {
-    case 2:
-      for (auto it2 = iter2;
-           it2 != std::sregex_iterator() && it2->str(1) == def; ++it2) {
-        std::cout << it2->str(2) << " ";
-      }
-      break;
-    case 3:
-      for (auto it2 = iter2; it2 != std::sregex_iterator() &&
-                             ((it2->str(1) == def) || (it2->str(1) == etym));
+      for (auto it2 = iter2; it2 != sregex_iterator() && ((it2->str(1) == def));
            ++it2) {
-        std::cout << it2->str(2) << " ";
+        file << it2->str(2) << " ";
       }
-      break;
+      // print word
+      for (auto it = iter; it != sregex_iterator(); ++it) {
+        if (i <= n) {
+          if (i != 0)
+            file << endl;
+          temp = it->str(1);
+          // another o(n)
+          for (int chars = 0; chars < temp.length(); chars++)
+            temp[chars] = tolower(temp[chars]);
+          dictionary.push_back(Word(temp, i));
+          i++;
+        }
+      }
     }
-  }
-  dict_file.close();
-  return 0;
-  }
-  else if (argc >= 2){
-    if (*argv[1] == 'd') {
-      unsigned int n = 100;
-      int i = 0;
-  std::string line_data;
-  auto dict_file = loadFile(filepath);
 
-  // no parameters
-  // getline has linear time O(n)
-  while (getline(dict_file, line_data) && i <= n) {
-    // get the word/phrase to process
-    std::sregex_iterator iter(line_data.begin(), line_data.end(), entry);
-    std::sregex_iterator iter2(line_data.begin(), line_data.end(), pattern);
-    // Create an iterator to find all matches in the input string
-    // regex_search is a O(n) operation on each processed line_data
-    // yes, this means O(n^2)
-    // it processes the regex expression using Thompsons algorithm
+    // O(N·log(N)) comparisons, where N is std::distance(first, last)
+    // in other words N = dictionary.size()
+    sort(dictionary.begin(), dictionary.end(), compareWord);
+    std::string input;
+    cout << "Dictionary sorted. Type a word to search:" << endl;
+    cin >> input;
 
-    // print word
-    for (auto it = iter; it != std::sregex_iterator(); ++it) {
-      if (i <= n) {
-        std::cout << it->str(1) << "\t";
-      }
-      i++;
-    }
-      for (auto it2 = iter2;
-           it2 != std::sregex_iterator() && it2->str(1) == def; ++it2) {
-        std::cout << it2->str(2) << " ";
-      }
-      for (auto it2 = iter2; it2 != std::sregex_iterator() &&
-                             ((it2->str(1) == def) || (it2->str(1) == etym));
+    int word_index = findWord(dictionary, input);
+    cout << searchDefinition(word_index) << endl;
+
+    dict_file.close();
+
+  } else if (argc >= 2) {
+    int n = *argv[1];
+    int i = 0;
+
+    string line_data;
+    string temp;
+    auto dict_file = loadFile(filepath);
+    ofstream file("dict.tsv");
+
+    // no parameters
+    // getline has linear time O(n)
+    while (getline(dict_file, line_data) && i < n) {
+      sregex_iterator iter(line_data.begin(), line_data.end(), entry);
+      sregex_iterator iter2(line_data.begin(), line_data.end(), pattern);
+
+      for (auto it2 = iter2; it2 != sregex_iterator() && ((it2->str(1) == def));
            ++it2) {
-        std::cout << it2->str(2) << " ";
+        file << it2->str(2) << " ";
       }
+      // print word
+      for (auto it = iter; it != sregex_iterator(); ++it) {
+        if (i <= n) {
+          if (i != 0)
+            file << endl;
+          temp = it->str(1);
+          // another o(n)
+          for (int chars = 0; chars < temp.length(); chars++)
+            temp[chars] = tolower(temp[chars]);
+          dictionary.push_back(Word(temp, i));
+          i++;
+        }
+      }
+    }
+
+    sort(dictionary.begin(), dictionary.end(), compareWord);
+
+    for (int i = 0; i < dictionary.size(); i++) {
+      cout << dictionary[i].index << " " << dictionary[i].word << ": ";
+      cout << searchDefinition(dictionary[i].index) << endl;
     }
     dict_file.close();
-    }
+  }
   return 0;
-  } 
 }
